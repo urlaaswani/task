76fb70b9c4ac9776ee6a732f85526e2a2af41360
urlaaswani <35735799+urlaaswani@users.noreply.github.com>
2019-01-31 18:51:13 +0530

    Updated main.js

--- main.js
+++ main.js
@@ -1,25 +1,3 @@
-// var array = [2, 10, 15, 11, 14, 13, 16];
-
-// for (var i = 0; i < array.length - 1; i++) 
-// {
-//     for (var j = i + 1; j < array.length - 1; j++) 
-//     {
-//         if (array[i] > array[j]) 
-//         {
-//             temp = array[i];
-//             array[i] = array[j];
-//             array[j] = temp;
-//         }
-//     }
-//     console.log(array[i] + ",");
-//     console.log(array);
-// }
-// // console.log("ascending Order:");
-// // for (var i = 0; i < array.length - 1; i++) 
-// // {
-  
-// // }
-// // console.log(array);
 
 
 var array =  [1, 2, 3, 4] ;
